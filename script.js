2b48b5dc4f800f561a03306122123eca318f5ebf
Mo. Saleh Gholami <iSalehGholami@gmail.com>
2024-03-04 13:50:24 +0330

    More design & functionality added to the project. Click to copy wallet and ...

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const explanations = [
     "because innovation drives our solutions."
 ];
 
+
+
 const mottoElement = document.getElementById('motto');
 let currentExplanationIndex = 0;
 let currentExplanation = explanations[currentExplanationIndex];
@@ -21,6 +23,28 @@ function typeWriter() {
     }
 }
 
+async function walletCardMaker() {
+    fetch("./wallets.json").then(response => response.json()).then(data => {
+        for (const { name, network, address } of data) {
+            walletCardWriter(`<div class="wallet">
+    <div class="wallet-name">${name}</div>
+    <div class="wallet-network">${network}</div>
+    <div class="wallet-address">${address}</div>
+</div>`);
+        }
+
+
+
+    }).catch(err => console.error("Error occured : " + err)).finally(async () => {
+        await eventAdder();
+
+    });
+}
+
+function walletCardWriter(walletComp) {
+    document.querySelector(".wallets-wrapper").innerHTML += walletComp;
+}
+
 function eraseExplanation() {
     if (charIndex > 0) {
         mottoElement.innerHTML = mottoElement.innerHTML.slice(0, -1);
@@ -33,5 +57,24 @@ function eraseExplanation() {
     }
 }
 
-// Start the typing animation
-typeWriter();
+async function eventAdder() {
+    let elements = document.querySelectorAll('.wallet-address');
+    for (const element of elements) {
+        ["click", "touchend", "touchcancel"].forEach(event => element.addEventListener(event, async (e) => {
+            await navigator.clipboard.writeText(element.innerHTML);
+            alert("Address was copied to your clipbaord")
+        }, false));
+    }
+
+}
+
+
+async function Main() {
+    // Start the typing animation
+    typeWriter();
+
+    await walletCardMaker();
+
+}
+
+Main();
